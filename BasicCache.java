541d9accf3c006ab0e1b4032ab38adddbd6a7315
Jan Paul Posma <jp.posma@gmail.com>
2011-03-09 13:38:53 +0100

    Simplified BasicCache

--- BasicCache.java
+++ BasicCache.java
@@ -1,36 +1,40 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
- * Basic FIFO caching class with a default size.
+ * Basic FIFO(-ish) caching class with a default size.
  */
 public class BasicCache implements ICache
 {
-	BasicCacheNode first, last;
-	HashMap<String, String> map;
-	int size;
+	LinkedList<String> list; // list to implement FIFO
+	HashMap<String, String> map; // map to look up values
+	int size; // size of the cache
 	
-	/**
-	 * Inits the cache with a certain size.
-	 */
 	public BasicCache(int size)
 	{
+		// size must be sane
+		if (size < 1) size = 1;
+		
+		// set the size of the cache
 		this.size = size;
 		
 		// Make sure the hashing algorithm is fast enough (0.8 is quite low),
 		// but make sure we will not reallocate (1.3*0.8 = 1.04)
 		map = new HashMap<String, String>( (int)(size * 1.30) , 0.8f);
+		
+		// build a simple linked list
+		list = new LinkedList<String>();
 	}
 	
-	/**
-	 * Default constructor, makes a cache with size 50.
-	 */
 	public BasicCache()
 	{
+		// default to size 50
 		this(50);
 	}
 	
 	public String get(String key)
 	{
+		// retrieve the value from the map
 		return map.get(key);
 	}
 	
@@ -40,42 +44,11 @@ public class BasicCache implements ICache
 		if (!map.containsKey(key))
 		{
 			// if the cache is full, remove the last element from the map (FIFO)
-			while (map.size() >= size - 1 && last.prev != null)
-			{
-				map.remove(last.key);
-				last = last.prev;
-			}
-			
-			if (first == null)
-			{
-				// if this is the first time a value is set, init first and last
-				first = new BasicCacheNode(key);
-				last = first;
-			}
-			else
-			{
-				// normally, store first, set the new one, and have the old one point
-				// to the new one
-				BasicCacheNode oldFirst = first;
-				first = new BasicCacheNode(key);
-				oldFirst.prev = first;
-			}
+			while (list.size() >= size) map.remove(list.removeLast());
+			list.addFirst(key);
 		}
-		map.put(key, val);
-	}
-	
-	/**
-	 * Simple linked list node class
-	 */
-	private class BasicCacheNode
-	{
-		public String key;
-		public BasicCacheNode prev;
 		
-		BasicCacheNode(String key)
-		{
-			this.key = key;
-			prev = null;
-		}
+		// store the value in the map
+		map.put(key, val);
 	}
 }
