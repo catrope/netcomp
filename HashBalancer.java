50f639d0b9e5e5e9c5dd24b214dbf1ec343225ab
Roan Kattouw <roan.kattouw@gmail.com>
2011-04-11 12:12:48 +0200

    Add server management to HashBalancer

--- HashBalancer.java
+++ HashBalancer.java
@@ -1,27 +1,53 @@
 import java.math.BigInteger;
 import java.security.*;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Vector;
 
 public class HashBalancer {
-	protected int n;
+	protected Vector<String> servers;
 	
-	public HashBalancer(int n) {
-		this.n = n;
+	public HashBalancer(Vector<String> servers) {
+		this.servers = new Vector<String>(servers);
 	}
 	
-	public int getIndexForKey(String key) {
+	public HashBalancer(String[] servers) {
+		this.servers = new Vector<String>(Arrays.asList(servers));
+	}
+	
+	public Vector<String> getServers() {
+		return servers;
+	}
+	
+	public int getNumberOfServers() {
+		return servers.size();
+	}
+	
+	public void addServer(String srv) {
+		servers.add(srv);
+	}
+	
+	public void removeServer(String srv) {
+		servers.remove(srv);
+	}
+	
+	public String getServerForKey(String key) {
+		return servers.get(getIndexForKey(key));
+	}
+	
+	protected int getIndexForKey(String key) {
 		try {
 			byte[] byteArr = key.getBytes("UTF-8");
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			byte[] hash = md.digest(byteArr);
 			BigInteger h = new BigInteger(1, hash);
-			return h.mod(BigInteger.valueOf(n)).intValue();
+			return h.mod(BigInteger.valueOf(servers.size())).intValue();
 		} catch(Exception e) {
 			// Java throws some stupid exceptions in case UTF-8 or MD5
 			// aren't supported. This is never realistically gonna happen,
 			// but just in case let's use a randomized balancer in that case.
 			Random generator = new Random();
-			return generator.nextInt(n);
+			return generator.nextInt(servers.size());
 		}
 	}
 }
--- HashBalancerTest.java
+++ HashBalancerTest.java
@@ -4,10 +4,18 @@ import junit.framework.TestCase;
 public class HashBalancerTest extends TestCase {
 
 	public void testGetIndexForKey() {
-		HashBalancer hb = new HashBalancer(16);
-		assertEquals(8, hb.getIndexForKey("foo"));
-		assertEquals(2, hb.getIndexForKey("bar"));
-		assertEquals(8, hb.getIndexForKey("baz"));
+		String[] servers = {"0", "1", "2", "3", "4", "5", "6",
+				"7", "8", "9", "a", "b", "c", "d", "e", "f"
+		};
+		
+		
+		HashBalancer hb = new HashBalancer(servers);
+		assertEquals("8", hb.getServerForKey("foo"));
+		assertEquals("2", hb.getServerForKey("bar"));
+		assertEquals("8", hb.getServerForKey("baz"));
+		assertEquals("e", hb.getServerForKey("Roan"));
+		assertEquals("f", hb.getServerForKey("Jan Paul"));
+		assertEquals("8", hb.getServerForKey("Roy"));
 	}
 
 }
