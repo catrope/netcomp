7e0756cb760fcd5d07a5de58cec5278f7afd5866
Roan Kattouw <roan.kattouw@gmail.com>
2011-04-11 13:08:09 +0200

    Document HashBalancer

--- HashBalancer.java
+++ HashBalancer.java
@@ -4,33 +4,66 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Vector;
 
+/**
+ * Manages a set of caching servers and load balances them using key hashing
+ * @author Roan Kattouw
+ */
 public class HashBalancer {
 	protected Vector<String> servers;
 	
+	/**
+	 * Constructor
+	 * @param servers Vector of server names
+	 */
 	public HashBalancer(Vector<String> servers) {
 		this.servers = new Vector<String>(servers);
 	}
 	
+	/**
+	 * Constructor
+	 * @param servers Array of server names
+	 */
 	public HashBalancer(String[] servers) {
 		this.servers = new Vector<String>(Arrays.asList(servers));
 	}
 	
+	/**
+	 * Get a list of all servers managed by this object
+	 * @return Vector of server names
+	 */
 	public Vector<String> getServers() {
 		return servers;
 	}
 	
+	/**
+	 * Get the number of servers managed by this object.
+	 */
 	public int getNumberOfServers() {
 		return servers.size();
 	}
 	
+	/**
+	 * Add a server to be managed by this object
+	 * @param srv Server name
+	 */
 	public void addServer(String srv) {
 		servers.add(srv);
 	}
 	
+	/**
+	 * Remove a server from this object
+	 * @param srv Server name
+	 */
 	public void removeServer(String srv) {
 		servers.remove(srv);
 	}
 	
+	/**
+	 * Find out which server should have a given key. This is the
+	 * hash-based load balancing part
+	 * @param key Cache key
+	 * @return Server name
+	 */
 	public String getServerForKey(String key) {
 		return servers.get(getIndexForKey(key));
 	}
