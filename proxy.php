331556c9044617ee8086da32867ffadc54c408a9
Roan Kattouw <roan.kattouw@gmail.com>
2011-04-11 14:47:55 +0200

    Strip embedded HTTP in proxy.php

--- proxy.php
+++ proxy.php
@@ -1,5 +1,10 @@
 <?php
 $sock = fsockopen($_REQUEST['srv'], $_REQUEST['prt']);
 fwrite($sock, $_REQUEST['meth'] . " " . $_REQUEST['pth'] . " HTTP/1.0\n\n");
-while(!feof($sock)) echo fread($sock, 100);
+
+$resp = '';
+while(!feof($sock)) $resp .= fread($sock, 100);
 fclose($sock);
+list(, $body) = explode("\n\n", $resp, 2);
+echo $body;
+
