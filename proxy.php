4e9cbf8b3c31028573e0744e775824107bd3365a
Roan Kattouw <roan.kattouw@gmail.com>
2011-04-11 14:24:35 +0200

    Add experimental proxy thingy

---
+++ proxy.php
@@ -0,0 +1,6 @@
+<?php
+echo "HEEEERE'S JOHNNY!\n\n";
+$sock = fsockopen($_REQUEST['srv'], $_REQUEST['prt']);
+fwrite($sock, $_REQUEST['meth'] . " " . $_REQUEST['pth'] . " HTTP/1.0\n\n");
+while(!feof($sock)) echo fread($sock, 100);
+fclose($fp);
