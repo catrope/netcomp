76a65e33ca365b44b7cde9c4769f58d468403869
Jan Paul Posma <jp.posma@gmail.com>
2011-04-11 11:52:56 +0200

    Added CacheServer, an RMI wrapper for the BasicCache

---
+++ CacheServer.java
@@ -0,0 +1,32 @@
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * RMI wrapper for a cache server
+ */
+public class CacheServer extends UnicastRemoteObject implements ICache
+{
+	final ICache localCache;
+	
+	public CacheServer(ICache localCache, String serverName) throws RemoteException
+	{
+		this.localCache = localCache;
+	}
+	
+	public void clear()
+	{
+		localCache.clear();
+	}
+	
+	public String get(String key)
+	{
+		return localCache.get(key);
+	}
+
+	public void set(String key, String val)
+	{
+		localCache.set(key, val);
+	}
+}
