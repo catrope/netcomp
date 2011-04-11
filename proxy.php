0f8f73b955e89835e451a41db343fa852e46926f
Roan Kattouw <roan.kattouw@gmail.com>
2011-04-11 14:49:58 +0200

    Implement support for POST bodies in proxy.php

--- proxy.php
+++ proxy.php
@@ -1,6 +1,8 @@
 <?php
+$content = isset($_REQUEST['cnt']) ? $_REQUEST['cnt'] : '';
 $sock = fsockopen($_REQUEST['srv'], $_REQUEST['prt']);
-fwrite($sock, $_REQUEST['meth'] . " " . $_REQUEST['pth'] . " HTTP/1.0\n\n");
+fwrite($sock, $_REQUEST['meth'] . " " . $_REQUEST['pth'] . " HTTP/1.0\n");
+if(strlen($content)) fwrite($sock, "Content-Length: " . strlen($content) . "\n\n$content\n");
 
 $resp = '';
 while(!feof($sock)) $resp .= fread($sock, 100);
